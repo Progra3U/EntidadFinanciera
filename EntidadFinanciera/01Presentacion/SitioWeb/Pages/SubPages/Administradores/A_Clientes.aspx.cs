7191e3ba57964f66161aba3e9f9cf71f014fb4c2
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-08-06 14:58:51 -0600

    Agregando Metodo Limpiar
    Se inicia creacion del metodo Limpiar para el form A_Clientes

--- EntidadFinanciera/01Presentacion/SitioWeb/Pages/SubPages/Administradores/A_Clientes.aspx.cs
+++ EntidadFinanciera/01Presentacion/SitioWeb/Pages/SubPages/Administradores/A_Clientes.aspx.cs
@@ -6,27 +6,42 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace _01Presentacion.SitioWeb.Pages.SubPages.Administradores
-{
+{   
+    
     public partial class A_Clientes : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        public void Limpiar()
         {
 
+            Cedula.Text = "";
+            Nombre.Text = "";
+            PrimerApellido.Text = "";
+            SegundoApellido.Text = "";
+            FechaNac.Text = "1";
+            Telefono.Text = "";
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
         }
 
         protected void Agregar_Click(object sender, EventArgs e)
         {
-
+            
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts","<script>alert('Guardado');</script>");
+            Limpiar();
         }
 
         protected void Actualizar_Click(object sender, EventArgs e)
         {
-
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Actualizado');</script>");
         }
 
         protected void Eliminar_Click(object sender, EventArgs e)
         {
-
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Eliminado');</script>");
         }
     }
 }
