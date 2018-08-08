b2f84b90609549693ceaf4bf78a299972079ab92
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-08-08 12:36:10 -0600

    Agregando Metodo para correos en A_Clientes
    Se agega metodo al fom

--- EntidadFinanciera/01Presentacion/SitioWeb/Pages/SubPages/Administradores/A_Clientes.aspx.cs
+++ EntidadFinanciera/01Presentacion/SitioWeb/Pages/SubPages/Administradores/A_Clientes.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Net;  //Librerias para envio de correo electronico
+using System.Net.Mail;
+using System.Threading; //Hilos de ejecucion
 
 namespace _01Presentacion.SitioWeb.Pages.SubPages.Administradores
 {   
@@ -47,6 +51,44 @@ namespace _01Presentacion.SitioWeb.Pages.SubPages.Administradores
         }
         #endregion
 
+        #region Correos
+        private void EnviarCorreoElectronicos()
+        {
+            SmtpClient envio = new SmtpClient(); //Configuracion del ENVIO 
+            MailMessage msj = new MailMessage(); //Configuracion del MSJ
+            string CorreoAEnviar = Correo.Text;
+            string Mensaje = "";
+
+            try
+            {
+                //Informacion del correo a enviar
+                msj = new MailMessage();
+                msj.To.Add(new MailAddress(CorreoAEnviar);  //modificar 
+                msj.From = new MailAddress("enviocorreos2019@gmail.com");
+                msj.Subject = "Apertura de Cuenta para "+ Nombre.Text; //modificar
+                msj.Body = "<b> Hola... Este es un correo de pruebas </b>"+ Mensaje; //modificar
+                msj.IsBodyHtml = true;
+                msj.Priority = MailPriority.Normal;
+
+                //Configuracion SMTP
+                envio.Host = "smtp.gmail.com";
+                envio.Port = 587;
+                envio.EnableSsl = true;
+                envio.UseDefaultCredentials = false;
+                envio.Credentials = new NetworkCredential("enviocorreos2019@gmail.com",
+                                                            "@UAM123456789");
+                //envio
+                envio.Send(msj);
+
+                msj.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
            
@@ -56,6 +98,9 @@ namespace _01Presentacion.SitioWeb.Pages.SubPages.Administradores
         {
             Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts","<script>alert('Guardado');</script>");
             Limpiar();
+
+            Thread hilo = new Thread(EnviarCorreoElectronicos);
+            hilo.Start(); //Inicia la ejecucion
         }
 
         protected void Actualizar_Click(object sender, EventArgs e)
