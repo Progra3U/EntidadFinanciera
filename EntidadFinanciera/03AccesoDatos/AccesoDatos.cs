a5729710ddac9edbeb5a0cbc8912179bb1e1f207
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-08-13 08:45:07 -0600

    Update
    Se agrega proyecto 00Servicios
    Se crean las referencias de los proyestos menos la referencia a servicios
    Esta pendiente Agregar el Entity ya que aun no esta lista la base de datos

--- EntidadFinanciera/03AccesoDatos/AccesoDatos.cs
+++ EntidadFinanciera/03AccesoDatos/AccesoDatos.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _04Entidades;
 
 namespace _03AccesoDatos
 {
-    public class Class1
+    public class AccesoDatos
     {
     }
 }
