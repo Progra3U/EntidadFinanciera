f5e7da51d7d7eb7c0f63f74bc556aad0a7f79b65
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-08-15 20:57:23 -0600

    Modificacion script db
    Modificacion script db

--- db/Scripts/EntidadFinanciera_tg_Transaccion.sql
+++ db/Scripts/EntidadFinanciera_tg_Transaccion.sql
@@ -1,13 +1,13 @@
 USE EntidadFinanciera 
 go
 --Creacion de Triggers para las tablas 
-CREATE TRIGGER tg_InsertTransaccion
+ALTER TRIGGER tg_InsertTransaccion
 ON Cliente
 FOR INSERT, UPDATE, DELETE
 AS
 INSERT Transaccion 
 (Cedula,CuentaInterna, CuentaSimpe, Descripcion, Monto, HorayFecha)
-SELECT Cedula, CuentaInterna, CuentaSimpe, Descripcion, 0, GETDATE()
+SELECT Cedula, CuentaInterna, CuentaSimpe, Descripcion, SaldoCuenta, GETDATE()
 FROM INSERTED
 
 
